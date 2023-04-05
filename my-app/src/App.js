14b9148078bfd4101f57f2fc3c0c08e9d6cac67f
AndypSheridan <andy.p.sheridan@gmail.com>
2023-04-05 17:02:19 +0000

    Update Set State in sfwps.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreeting /> */}
       {/* FunctionalGreetingWithProps greeting="Nice to meet you!" name="Andy" */}
-      <StatefulGreeting greeting="I'm a stateful class component!" />
+      <StatefulGreetingWithPrevState />
     </div>
   );
 }
