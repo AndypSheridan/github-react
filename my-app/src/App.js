706d6b557e537b636c06ae8085605f2bec001385
AndypSheridan <andy.p.sheridan@gmail.com>
2023-04-09 15:29:31 +0000

    Add conditional rendering files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 // import StatefulGreeting from './components/StatefulGreeting';
 // import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+// import EventsFunctional from './components/EventsFunctional';
+// import EventsClass from './components/EventsClass';
+// import EventBinding from './components/EventBinding';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreeting /> */}
       {/* FunctionalGreetingWithProps greeting="Nice to meet you!" name="Andy" */}
-      <StatefulGreetingWithPrevState />
+      {/* <StatefulGreetingWithPrevState /> */}
+      {/* <EventsFunctional />
+      <EventsClass /> */}
+      {/* <EventBinding /> */}
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional />
     </div>
   );
 }
