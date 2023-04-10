5d1df0c2a966267dc8cb3f5e02a23ccced20b35d
AndypSheridan <andy.p.sheridan@gmail.com>
2023-04-10 15:49:27 +0000

    Add unique key to elements

--- my-app/src/components/Book.js
+++ my-app/src/components/Book.js
@@ -1,40 +1,17 @@
 import React from 'react'
 import RenderingLists from './RenderingLists'
 
-function Book() {
+function Book(props) {
 
-    const books = [
-        {
-            title: "to kill a mockingbird",
-            author: "harper lee",
-            pages: 281
-        },
-        {
-            title: "the great gatsby",
-            author: "f. scott fitzgerald",
-            pages: 218
-        },
-        {
-            title: "the catcher in the rye",
-            author: "j.d. salinger",
-            pages: 234
-        }
-    ]
+    const book=props.book
 
     return (
         <div>
-            {
-                books.map(book => {
-                    return <div>
-                        <h5>Title: {book.title.toUpperCase()}</h5>
-                        <p>Author: {book.author}</p>
-                        <p>Number of pages: {book.pages}</p>
-                    </div>
-
-                })
-            }
+            <h5>Title: {book.title.toUpperCase()}</h5>
+            <p>Author: {book.author}</p>
+            <p>Number of pages: {book.pages}</p>
         </div>
-    )
+        )
 }
 
 export default Book
