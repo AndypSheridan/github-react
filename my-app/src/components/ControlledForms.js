db30ea441f56725349ff19918fb323285e94e8c8
AndypSheridan <andy.p.sheridan@gmail.com>
2023-04-12 12:27:17 +0000

    Add form submission prevent default

--- my-app/src/components/ControlledForms.js
+++ my-app/src/components/ControlledForms.js
@@ -6,32 +6,98 @@ export class ControlledForms extends Component {
         super(props)
 
         this.state = {
-            name: ""
+            name: "",
+            category: "website",
+            comments: "",
         }
     }
+
+
+    // computed property name
+    // handleChange = (event) => {
+    //     const {name, value} =event.target
+    //     this.setState({
+    //         [name]: value
+    //     })
+    // }
 
     handleNameChange = (event) => {
         this.setState({
             name: event.target.value
-        // let newValue = event.target.value
-        // console.log(newValue)
-        // this.setState({
-        //     name: newValue
+
+            // or....
+            // let newValue = event.target.value
+            // console.log(newValue)
+            // this.setState({
+            //     name: newValue
         })
+    }
+
+    handleCategoryChange = (event) => {
+        this.setState({
+            category: event.target.value
+
+            // or....
+            // let newValue = event.target.value
+            // console.log(newValue)
+            // this.setState({
+            //     name: newValue
+        })
+    }
+
+    handleCommentsChange = (event) => {
+        this.setState({
+            comments: event.target.value
+
+            // or....
+            // let newValue = event.target.value
+            // console.log(newValue)
+            // this.setState({
+            //     name: newValue
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state)
     }
 
     render() {
         return (
             <div>
-                <form>
-                    <label htmlFor='id-name'>Your Name: </label>
-                    <input 
-                        value={this.state.name}
-                        onChange={this.handleNameChange}
-                        id='id-name'
-                        name='name' 
-                        type="text" 
-                    />
+                <h2>Please fill out the form below</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor='id-name'>Your Name: </label>
+                        <input
+                            value={this.state.name}
+                            onChange={this.handleNameChange}
+                            id='id-name'
+                            name='name'
+                            type="text"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='id-category'>Query Category: </label>
+                        <select 
+                            id='id-category' 
+                            name='category'
+                            value={this.state.category}
+                            onChange={this.handleCategoryChange}>
+                            <option value='website'>Website issue</option>
+                            <option value='order'>Order issue</option>
+                            <option value='general'>General inquiry</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor='id-comments'>Comments: </label>
+                        <textarea 
+                            id='id-comments' 
+                            name='comments' 
+                            value={this.state.comments}
+                            onChange={this.handleCommentsChange}
+                            />
+                    </div>
                     <input type="submit" value="Submit" />
                 </form>
             </div>
